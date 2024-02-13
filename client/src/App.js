933dba5364549a5537867d8bc72bf001531770c7
Guzal-Rustamova <111534805+Guzal-Rustamova@users.noreply.github.com>
2024-02-13 13:30:49 -0600

    getting a lot of weird errors

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,28 @@ import {
   Routes, 
   Route, 
 } from "react-router-dom"; 
+import Home from "./pages"; 
 import About from "./pages/about"; 
+import ContactUs from "./pages/contact";
+import Help from "./pages/help"; 
 
 
 function App() {
-  return (
-    <body>
+    return (
+    <Router> 
+      <div>
+      <Navbar />
+      <Routes> 
+        <Route exact path = "/" element = {<Home />} /> 
+        <Route path = "/about" element = {<About /> } />
+        <Route 
+          path = "/contact" 
+          element = {<ContactUs />} 
+        />
+        <Route path = "/help" element = {<Help /> } />
+      </Routes>
+      </div>
+      <body>
       <div className="App">
         <header className="App-header">
           
@@ -32,6 +48,10 @@ function App() {
         </header>
       </div>
     </body>
+    </Router>
+    
+    
+    
         
    
         
