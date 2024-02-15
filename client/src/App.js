ce6bcc251e7f9a020f6abca1b9aaf331f1dce1c8
Guzal-Rustamova <111534805+Guzal-Rustamova@users.noreply.github.com>
2024-02-15 13:30:37 -0600

    resolved all bugs and conflicts, but the website is displaying a blank screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,37 @@
 import Logo from './logo.png';
 import './App.css';
+import Navbar from "./components/Navbar";
 import React from "react"; 
-import Navbar from "./components/Navbar"; 
 import {
   BrowserRouter as Router, 
   Routes, 
   Route, 
 } from "react-router-dom"; 
-import Home from "./pages/home"; 
-import About from "./pages/about"; 
-import ContactUs from "./pages/contact";
-import Help from "./pages/help"; 
+import Home from "./pages/Home"; 
+import About from "./pages/About"; 
+import ContactUs from "./pages/Contact";
+import Help from "./pages/Help"; 
+import ReactDOM from 'react-dom/client';
 
+
+ReactDOM.render (
+
+  <Router> 
+      <Navbar /> 
+      <Routes> 
+          <Route path = "/" element = {<Home/>} />
+          <Route path = "/about" element = {<About />} />
+          <Route path = "/contact" element = {<ContactUs />} />
+          <Route path = "/help" element = {<Help />} />
+      </Routes>
+  </Router>,
+
+  document.getElementById("root")
+);
 
 function App() {
     return (
-    <Router> 
-      <body>
-      <div>
-      <Navbar />
-      <Routes> 
-        <Route path = "/" element = <Home /> /> 
-        <Route path = "/About" element = <About />  />
-        <Route 
-          path = "/Contact" 
-          element = <ContactUs />
-        />
-        <Route path = "/Help" element = {<Help /> } />
-      </Routes>
-      </div>
+    
       
       <div className="App">
         <header className="App-header">
@@ -48,8 +51,7 @@ function App() {
           </div>
         </header>
       </div>
-    </body>
-    </Router>
+  
     
     
     
