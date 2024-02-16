d44e08f3da9823dac5ff1f1a706c26903aabfd1b
Guzal-Rustamova <111534805+Guzal-Rustamova@users.noreply.github.com>
2024-02-16 13:18:30 -0600

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,14 @@
 import Logo from './logo.png';
 import './App.css';
-import Navbar from "./components/Navbar";
 import React from "react"; 
-import {
-  BrowserRouter as Router, 
-  Routes, 
-  Route, 
-} from "react-router-dom"; 
-import Home from "./pages/Home"; 
-import About from "./pages/About"; 
-import ContactUs from "./pages/Contact";
-import Help from "./pages/Help"; 
-import ReactDOM from 'react-dom/client';
 
-
-ReactDOM.render (
-
-  <Router> 
-      <Navbar /> 
-      <Routes> 
-          <Route path = "/" element = {<Home/>} />
-          <Route path = "/about" element = {<About />} />
-          <Route path = "/contact" element = {<ContactUs />} />
-          <Route path = "/help" element = {<Help />} />
-      </Routes>
-  </Router>,
-
-  document.getElementById("root")
-);
 
 function App() {
-    return (
+  return (
+    <>
+   
+
     
-      
       <div className="App">
         <header className="App-header">
           
@@ -51,13 +27,9 @@ function App() {
           </div>
         </header>
       </div>
-  
+    </>
     
     
-    
-        
-   
-        
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter, 
+  Routes, 
+  Route, 
+} from "react-router-dom"; 
+import Home from "./pages/Home"; 
+import About from "./pages/About"; 
+import ContactUs from "./pages/Contact";
+import Help from "./pages/Help"; 
+import Navbar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <>
+  <BrowserRouter> 
+  <Navbar /> 
+    <Routes> 
+        <Route path = "/" element = {<Home/>} />
+        <Route path = "/about" element = {<About />} />
+        <Route path = "/contact" element = {<ContactUs />} />
+        <Route path = "/help" element = {<Help />} />
+    </Routes>
+    </BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
