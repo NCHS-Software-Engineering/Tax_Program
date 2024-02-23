13851c6432583a485cd809abc9820de6f54ed15d
Guzal-Rustamova <111534805+Guzal-Rustamova@users.noreply.github.com>
2024-02-23 13:18:24 -0600

    commented out all the webstite route

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,11 @@
 import Logo from './logo.png';
 import './App.css';
 import React from "react"; 
-import Root from "./routes/root";
-import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
 
 function App() {
   return (
-    <>
-   
-
-    
+  
       <div className="App">
         <header className="App-header">
           
@@ -45,7 +24,7 @@ function App() {
           </div>
         </header>
       </div>
-    </>
+
     
     
   );
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+/*
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import AboutPage from './pages/About';
+import ContactPage from './pages/Contact'
+import HelpPage from './pages/Help'
 
 
 const router = createBrowserRouter([
@@ -19,15 +31,32 @@ const router = createBrowserRouter([
     path: "about",
     element: <AboutPage/>,
   },
+  {
+    path: "contact",
+    element: <ContactPage/>,
+  },
+  {
+    path: "help",
+    element: <HelpPage/>,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="help" element={<HelpPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
