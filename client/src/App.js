6a7e8416e231c4bf702975d83dfbb7974f44975c
Guzal-Rustamova <111534805+Guzal-Rustamova@users.noreply.github.com>
2024-02-27 13:30:43 -0600

    fixed errors in the server, now working on connecting the information from the database to the table in the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,42 @@
 import Logo from './logo.png';
 import './App.css';
 import React from "react"; 
+import React, { useState, useEffect } from "react";
 
+async function postData(url = "", data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
 
 function App() {
+
+  const baseURL = "http://localhost:3000/";
+  const[income, setIncome] = useState([]); 
+  useEffect(() => {
+    fetch(`${baseURL}branches`)
+      .then((res) => res.json())
+      .then((data) => {setBranches(data);}
+      );
+  }, []);
+
+
+
+
+
+
   return (
   
       <div className="App">
@@ -15,10 +48,13 @@ function App() {
           <div className = "Tax brackers"> 
             <table> 
             <tr> 
-              <th>Income</th>
+              <th>Minimum Income</th>
+              <th>Maximum Income</th>
               <th>Household Status</th>
-              <th>Tax Owed</th>
             </tr>
+            {
+              
+            }
             </table> 
         
           </div>
