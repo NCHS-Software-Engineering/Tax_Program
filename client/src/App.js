4ad2e8b07ca49a31416d98df9866a9d794eb38b7
MatthewScoles <msscoles@stu.naperville203.org>
2024-03-04 13:11:49 -0600

    e

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,48 @@
-import logo from './logo.png';
 import './App.css';
 import React from "react";
-import Navbar from "./components/Navbar";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-} from "react-router-dom";
-import Home from "./pages";
-import About from "./pages/About";
-import Blogs from "./pages/Help";
-import Contact from "./pages/Contact";
- 
-/*function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route
-                    path="/contact"
-                    element={<Contact />}
-                />
-                <Route path="/blogs" element={<Help />} />
-            </Routes>
-        </Router>
-    );
-}
- 
-export default App;
 
-*/function App() {
+import Income from './Components/Incomemain/Income.jsx';
+
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Income />
     </div>
   );
 }
+
+
+/*function App() {
+  return (
+    <body>
+      <div className="App">
+        <header className="App-header">
+          
+            <img id = "left" src={logo} alt="logo" />
+            <h1>Tax Program</h1>
+            <img id = "right" src={logo} alt="logo" />
+           
+
+        
+            <form name = 'income'>
+  <div>
+    <label> What is your annual income (0-100k):</label>
+    Income: <input
+      type="number"
+      name="income"
+      min="0"
+      max="100000"
+      required />
+    <span class="validity"></span>
+    <input type="submit" onclick='check(); return false' />
+  </div>
+</form>
+
+
+        </header>
+      </div>
+    </body>
+  );
+}*/
 
 export default App;
